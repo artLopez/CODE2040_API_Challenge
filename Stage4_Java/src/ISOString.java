54e1387e85057f9061d785e6204f8e9cdd3b33e4
Arturo Lopez <arlopez@csumb.edu>
2014-11-26 22:19:13 -0800

    Added comments for Stage 4

--- Stage4_Java/src/ISOString.java
+++ Stage4_Java/src/ISOString.java
@@ -1,3 +1,17 @@
+/**
+ * Title: ISOString.java
+ * Abstract: The ISOString class 
+ * acquires the interval and datestamp.
+ * Afterwards, the datestamp is parsed
+ * and adds the interval in seconds using
+ * the Class Calendar.
+ * 
+ * 
+ * Author: Arturo Lopez
+ * ID:2412
+ * Date: October 1, 2014
+ */
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -18,7 +32,7 @@ import java.util.Set;
 
 @SuppressWarnings("deprecation")
 public class ISOString {
- 
+	//Variable Declaration
     private String token;
     private String apiURL;
     private static int interval = 0;
@@ -31,21 +45,24 @@ public class ISOString {
     private static int seconds = 0;
     private static String finalISO = null;
     
+    //Constructor for the ISOString class
     public ISOString(String token, String apiURL) {        
         this.apiURL = apiURL;
         this.token = token;
     }
-    
+    //sends a post request with the updated datestamp
     public void sendHTTPPOST()
     {
     	 try {
              @SuppressWarnings("deprecation")
  			HttpClient send = new DefaultHttpClient();        
             HttpPost post = new HttpPost("http://challenge.code2040.org/api/validatetime");        
-  
-             post.setEntity(new StringEntity("{\"token\":\"" + this.token + "\" , \"datestamp\":\"" + finalISO + "\"}", ContentType.create("application/json")));
-  
+            //post sent with the dictionary {"token" : myToken, "datestamp": finalISO(datestamp}
+             post.setEntity(new StringEntity("{\"token\":\"" + token + "\" , \"datestamp\":\"" + finalISO + "\"}", ContentType.create("application/json")));
+             
+             //response for the post request
              HttpResponse response = send.execute(post);
+             //prints out the status code from the post request
              System.out.println("Status: "+ response.getStatusLine().getStatusCode());
          }
          catch(IOException e) {
@@ -53,25 +70,27 @@ public class ISOString {
          }   
     	
     }
- 
+    //sends a post request to acquire the datestamp and interval
     public void makeHTTPPOSTRequest() {
         try {
             @SuppressWarnings("deprecation")
 			HttpClient send = new DefaultHttpClient();        
             HttpPost post = new HttpPost(this.apiURL);        
- 
-            post.setEntity(new StringEntity("{\"token\":\"" + this.token + "\"}", ContentType.create("application/json")));
+            //post request sending the token to acquire the datestamp and interval
+            post.setEntity(new StringEntity("{\"token\":\"" + token + "\"}", ContentType.create("application/json")));
  
             HttpResponse response = send.execute(post);
-          
+            
+            //bufferedReader to parse the JSON dictionary
             BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
             String line = "";
-            while ((line = rd.readLine()) != null) {
-               //Parse our JSON response               
+            //reading the JSON dictionary
+            while ((line = rd.readLine()) != null) {               
                JSONParser dateParsing = new JSONParser();
                JSONObject result = (JSONObject)dateParsing.parse(line);
-             
+              //print the JSON dictionary
               System.out.println(result);
+              //parse the dictionary
               parseJson(result);
             }
         }
@@ -85,7 +104,7 @@ public class ISOString {
     }    
     
     public static void parseJson(JSONObject jsonObject) throws ParseException {
-
+    	//iterating through the dictionary to acquire the datestamo and interval
         Set<Object> set = jsonObject.keySet();
         Iterator<Object> iterator = set.iterator();
         while (iterator.hasNext()) {
@@ -94,40 +113,47 @@ public class ISOString {
             	parseJson((JSONObject) jsonObject.get(obj));
             else
             {
+            	//print out the keys that are getting parsed 
             	System.out.println(obj.toString() + "\t" + jsonObject.get(obj));
+            	//checks the keys by comparing string to store the data in the right variable
             	if(obj.toString().equals("interval"))
-            	{
             		interval = Integer.parseInt(jsonObject.get(obj).toString());
-            	}
             	else if(obj.toString().equals("datestamp"))
-            	{
             		date = jsonObject.get(obj).toString();
-            	}
             }
           }
         }
  
 	public static void updateDate()
     {
+		//create a Calendar instance given the year, month,days,hours,minutes and seconds
 		SimpleDateFormat createDate = new SimpleDateFormat("yyyy MMM dd HH:mm:ss");
 		Calendar calendar = new GregorianCalendar(year,month - 1,days,hours,minutes,seconds);
 				
+		//print out the current date
 		System.out.println("Date : " + createDate.format(calendar.getTime()));
+		
+		//upgrade the current date with the interval to acquire the final datestamp
 		calendar.add(Calendar.SECOND, interval);
-				
+		
+		//input the the new date into the finalISO String
 		setFinalISO(createDate.format(calendar.getTime()));
+		//print out updated date
 		System.out.println("Date : " + createDate.format(calendar.getTime()));
+		//print out finalISO String
 		System.out.println(getFinalISO());
 				
+		//Updating the final datestamp into ISO 8601 format
 		SimpleDateFormat finalDate = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 		String date = finalDate.format(calendar.getTime()) + ".000Z" ;
-		System.out.println(date); 
 		date = date.substring(0, 10) + "T" + date.substring(11,24);
+		//print date
 		System.out.println(date); 
     }
 	
 	public static void parseISO(String dateStamp)
 	{
+		//parsing the datestamp to seperate years,month,days,hours,minutes,seconds
 		year = Integer.parseInt(dateStamp.substring(0,4));
 		month = Integer.parseInt(dateStamp.substring(5, 7));
 		days = Integer.parseInt(dateStamp.substring(8, 10));
--- Stage4_Java/src/StageIV.java
+++ Stage4_Java/src/StageIV.java
@@ -1,7 +1,23 @@
+/**
+ * Title: StageIV.java
+ * Abstract: Create an instance 
+ * of the ISOString class with
+ * parameters containing the token
+ * and URL.Then calls post request
+ * to acquire the datestamp and the
+ * interval to add to the datestamp.
+ * 
+ * 
+ * Author: Arturo Lopez
+ * ID:2412
+ * Date: October 1, 2014
+ */
+
 public class StageIV {
 	public static void main(String[] args) {
 		ISOString test = new ISOString("dLcGAO9jnJ","http://challenge.code2040.org/api/time");
 		test.makeHTTPPOSTRequest();
+		//Sends back the updated datestamp
 		test.sendHTTPPOST();
 	}
 
